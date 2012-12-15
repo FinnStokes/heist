2eb2425fdc94e54466da536dfab4d0e0348342ab
Finn Stokes <finn.stokes@gmail.com>
2012-12-15 18:42:54 +1030

    Added acceleration to physics system

--- physics.lua
+++ physics.lua
@@ -8,6 +8,10 @@ system.add(M)
 
 M.step = function (dt, ents)
   for _,e in ipairs(ents) do
+    if e.velocity and e.acceleration then
+      e.velocity.x = e.velocity.x + e.acceleration.x * dt
+      e.velocity.y = e.velocity.y + e.acceleration.y * dt
+    end
     if e.position and e.velocity then
       e.position.x = e.position.x + e.velocity.x * dt
       e.position.y = e.position.y + e.velocity.y * dt
