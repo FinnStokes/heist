6df475b329316b6c082434640a477324a43b9e29
Saxon Douglass <saxon@saxondouglass.com>
2012-12-18 12:20:03 +1030

    Created final level for jam submission

--- data/img/level.lua
+++ data/img/level.lua
@@ -1,22 +1,115 @@
-return { version = "1.1", luaversion = "5.1", orientation = "orthogonal", width = 20, height = 20, tilewidth = 16, tileheight = 16, properties = {}, tilesets = { { name = "tileset", firstgid = 1, tilewidth = 16, tileheight = 16, spacing = 0, margin = 0, image = "tileset.png", imagewidth = 256, imageheight = 256, properties = {}, tiles = { { id = 0, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 1, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 2, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 3, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 16, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 17, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 18, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 19, properties = { ["opaque"] = "true", ["solid"] = "true" } } } } }, layers = { { type = "tilelayer", name = "Tile Layer 1", x = 0, y = 0, width = 20, height = 20, visible = true, opacity = 1, properties = {}, encoding = "lua", data = { 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 3, 19, 19, 19, 19, 19, 4, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 2, 5, 5, 5, 5, 5, 2, 66, 66, 66, 66, 66, 3, 19, 19, 19, 19, 4, 66, 66, 2, 5, 5, 5, 5, 5, 2, 66, 66, 66, 66, 66, 2, 5, 5, 5, 5, 2, 19, 19, 20, 5, 1, 5, 5, 5, 2, 66, 66, 66, 1, 19, 20, 5, 5, 5, 5, 2, 5, 5, 5, 5, 18, 4, 5, 5, 2, 66, 66, 66, 2, 5, 5, 5, 1, 5, 5, 2, 5, 5, 5, 5, 5, 2, 5, 5, 18, 19, 19, 4, 2, 5, 5, 5, 2, 5, 5, 2, 5, 5, 5, 5, 5, 2, 5, 5, 5, 5, 5, 2, 2, 19, 20, 5, 2, 5, 5, 17, 5, 5, 3, 19, 19, 2, 5, 18, 20, 5, 5, 2, 2, 5, 5, 5, 2, 5, 5, 5, 5, 5, 17, 5, 5, 17, 5, 5, 5, 5, 5, 2, 2, 5, 5, 5, 2, 5, 5, 17, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 2, 5, 5, 5, 17, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 19, 19, 20, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 66, 66, 66, 18, 19, 4, 5, 5, 5, 5, 5, 1, 5, 5, 5, 5, 5, 5, 5, 2, 66, 66, 66, 66, 66, 18, 19, 4, 5, 5, 5, 17, 5, 1, 5, 5, 5, 5, 3, 20, 66, 66, 66, 66, 66, 66, 66, 2, 5, 5, 5, 5, 5, 2, 5, 18, 20, 5, 2, 66, 66, 66, 66, 66, 66, 66, 66, 18, 19, 4, 5, 5, 5, 2, 5, 5, 5, 5, 2, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 18, 19, 19, 19, 19, 19, 19, 19, 19, 20, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66 } }, { type = "objectgroup", name = "Object Layer 1", visible = true, opacity = 1, properties = {}, objects = { { name = "", type = "spawner", x = 32, y = 176, width = 16, height = 16, properties = {} }, { name = "speccyGit", type = "guard", x = 144, y = 160, width = 0, height = 0, polygon = { { x = 0, y = 0 },
-            { x = 0, y = -48 },
-            { x = 48, y = -48 },
-            { x = 48, y = -64 },
-            { x = 32, y = -64 },
-            { x = 32, y = -112 },
-            { x = 64, y = -112 },
-            { x = 64, y = -96 },
-            { x = 80, y = -96 },
-            { x = 80, y = -48 },
-            { x = 144, y = -48 },
-            { x = 144, y = 0 },
-            { x = 80, y = 0 },
-            { x = 80, y = 64 },
-            { x = 48, y = 64 },
+return { version = "1.1", luaversion = "5.1", orientation = "orthogonal", width = 30, height = 30, tilewidth = 16, tileheight = 16, properties = {}, tilesets = { { name = "tileset", firstgid = 1, tilewidth = 16, tileheight = 16, spacing = 0, margin = 0, image = "tileset.png", imagewidth = 256, imageheight = 256, properties = {}, tiles = { { id = 0, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 1, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 2, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 3, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 16, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 17, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 18, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 19, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 20, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 21, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 22, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 23, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 24, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 25, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 26, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 27, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 32, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 33, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 34, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 35, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 36, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 37, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 38, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 39, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 40, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 41, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 42, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 43, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 48, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 49, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 50, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 51, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 52, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 53, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 54, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 55, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 64, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 68, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 80, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 84, properties = { ["opaque"] = "false", ["solid"] = "true" } }, { id = 96, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 97, properties = { ["opaque"] = "true", ["solid"] = "true" } }, { id = 112, properties = { ["opaque"] = "true", ["solid"] = "false" } }, { id = 113, properties = { ["opaque"] = "true", ["solid"] = "false" } } } } }, layers = { { type = "tilelayer", name = "tiles", x = 0, y = 0, width = 30, height = 30, visible = true, opacity = 1, properties = {}, encoding = "lua", data = { 3, 38, 39, 38, 1, 23, 38, 23, 23, 38, 23, 1, 23, 38, 39, 23, 23, 38, 23, 1, 19, 35, 19, 19, 35, 19, 19, 35, 19, 4, 2, 8, 8, 8, 2, 8, 8, 8, 8, 8, 8, 2, 8, 8, 8, 8, 8, 8, 8, 2, 50, 51, 51, 52, 6, 50, 51, 51, 52, 2, 2, 8, 8, 8, 37, 8, 8, 8, 8, 8, 8, 2, 8, 8, 8, 8, 8, 8, 8, 2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2, 2, 8, 8, 8, 8, 8, 8, 22, 38, 23, 23, 38, 23, 40, 38, 24, 8, 8, 22, 2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2, 2, 8, 8, 8, 1, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 6, 6, 18, 35, 19, 35, 20, 6, 6, 2, 2, 8, 8, 8, 2, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2, 2, 23, 40, 23, 2, 55, 55, 55, 56, 7, 7, 54, 55, 55, 55, 55, 56, 7, 7, 2, 6, 6, 49, 6, 6, 6, 49, 6, 6, 2, 2, 8, 8, 8, 37, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 6, 6, 65, 6, 6, 6, 65, 6, 6, 2, 2, 8, 8, 8, 8, 7, 7, 22, 38, 23, 24, 7, 7, 7, 7, 7, 7, 7, 7, 2, 6, 6, 81, 6, 6, 6, 81, 6, 6, 2, 2, 8, 8, 8, 1, 7, 7, 7, 7, 7, 7, 7, 7, 22, 23, 38, 24, 7, 7, 2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2, 2, 38, 23, 38, 24, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 6, 6, 3, 35, 19, 19, 36, 5, 5, 2, 2, 53, 7, 7, 7, 7, 7, 53, 7, 1, 7, 7, 7, 7, 7, 7, 7, 7, 7, 2, 6, 6, 2, 5, 5, 5, 5, 5, 5, 2, 2, 69, 7, 7, 53, 7, 7, 69, 7, 2, 55, 56, 7, 7, 53, 7, 7, 7, 7, 2, 6, 6, 2, 5, 5, 5, 5, 5, 5, 2, 2, 85, 7, 7, 69, 7, 7, 69, 7, 2, 7, 7, 7, 7, 69, 7, 7, 7, 7, 2, 6, 6, 2, 5, 5, 5, 5, 5, 5, 2, 2, 23, 23, 23, 24, 7, 7, 69, 7, 2, 7, 7, 7, 7, 69, 7, 7, 54, 55, 2, 6, 6, 2, 5, 5, 5, 18, 35, 20, 2, 2, 7, 7, 7, 7, 7, 7, 85, 7, 2, 55, 56, 7, 7, 69, 7, 7, 7, 7, 2, 6, 6, 33, 6, 6, 6, 6, 6, 6, 2, 2, 7, 7, 7, 7, 7, 7, 7, 7, 2, 7, 7, 7, 7, 69, 7, 7, 7, 7, 33, 6, 6, 6, 6, 49, 6, 6, 6, 6, 2, 2, 7, 7, 22, 38, 39, 23, 38, 23, 23, 23, 38, 23, 40, 23, 38, 24, 7, 7, 6, 6, 6, 6, 6, 65, 6, 6, 6, 6, 2, 2, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 81, 6, 6, 6, 6, 2, 2, 42, 43, 44, 1, 97, 27, 27, 43, 27, 27, 27, 27, 27, 42, 27, 27, 27, 27, 44, 27, 27, 27, 43, 28, 9, 9, 26, 98, 2, 2, 10, 10, 10, 2, 113, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 114, 2, 2, 10, 10, 10, 2, 9, 26, 42, 27, 27, 27, 44, 28, 9, 3, 97, 27, 42, 44, 43, 27, 98, 4, 9, 9, 9, 9, 9, 9, 2, 2, 10, 10, 10, 2, 9, 9, 9, 9, 9, 9, 9, 9, 9, 2, 113, 9, 9, 9, 9, 9, 114, 2, 9, 9, 9, 9, 9, 9, 2, 2, 28, 10, 26, 42, 27, 27, 4, 9, 9, 9, 9, 26, 98, 2, 9, 9, 9, 9, 9, 9, 9, 2, 9, 9, 9, 9, 9, 9, 2, 2, 9, 9, 9, 9, 9, 9, 2, 9, 9, 9, 9, 9, 114, 2, 9, 9, 9, 1, 9, 9, 9, 2, 9, 9, 9, 9, 9, 9, 2, 2, 9, 9, 9, 9, 9, 9, 41, 9, 26, 27, 27, 27, 27, 28, 9, 9, 9, 2, 9, 9, 9, 26, 27, 27, 42, 27, 27, 98, 2, 2, 97, 28, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 2, 9, 9, 9, 9, 9, 9, 9, 9, 9, 114, 2, 2, 113, 9, 9, 9, 9, 9, 26, 27, 27, 27, 42, 27, 27, 27, 27, 43, 27, 27, 27, 44, 28, 9, 9, 9, 9, 9, 9, 9, 2, 2, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 2, 26, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 28 } }, { type = "objectgroup", name = "objects", visible = true, opacity = 1, properties = {}, objects = { { name = "", type = "spawner", x = 32, y = 128, width = 16, height = 16, properties = {} }, { name = "", type = "guard", x = 176, y = 112, width = 0, height = 0, polygon = { { x = 0, y = 0 },
+            { x = -80, y = 0 },
+            { x = -80, y = 32 },
+            { x = 16, y = 32 },
+            { x = 16, y = 16 },
+            { x = 96, y = 16 },
+            { x = 96, y = 48 },
+            { x = 0, y = 48 }
+          },
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 96,
+          y = 256,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = 0, y = -96 },
+            { x = 32, y = -96 },
+            { x = 32, y = 0 }
+          },
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 208,
+          y = 256,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = 0, y = -80 },
+            { x = 32, y = -80 },
+            { x = 32, y = 0 },
+            { x = 48, y = 0 },
+            { x = 48, y = -80 },
+            { x = -16, y = -80 },
+            { x = -16, y = 0 }
+          },
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 208,
+          y = 32,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = 48, y = 0 },
             { x = 48, y = 32 },
-            { x = -32, y = 32 },
-            { x = -32, y = 80 },
-            { x = 0, y = 80 }
+            { x = -112, y = 32 },
+            { x = -112, y = 0 },
+            { x = -64, y = 0 },
+            { x = -64, y = -16 },
+            { x = -128, y = -16 },
+            { x = -128, y = 48 },
+            { x = 64, y = 48 },
+            { x = 64, y = -16 },
+            { x = 0, y = -16 }
+          },
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 416,
+          y = 256,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = 32, y = 0 },
+            { x = 32, y = 32 },
+            { x = 0, y = 32 }
+          },
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 400,
+          y = 80,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = 0, y = 64 },
+            { x = -32, y = 64 },
+            { x = -32, y = 0 }
+          },
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 432,
+          y = 192,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = -64, y = 0 },
+            { x = -64, y = 64 },
+            { x = -96, y = 64 },
+            { x = -96, y = -144 },
+            { x = 0, y = -144 }
           },
           properties = {}
         },
@@ -24,9 +117,92 @@ return { version = "1.1", luaversion = "5.1", orientation = "orthogonal", width 
           name = "",
           type = "goal",
           x = 32,
-          y = 96,
+          y = 336,
           width = 16,
           height = 16,
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 432,
+          y = 368,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = -48, y = 0 },
+            { x = -48, y = -32 },
+            { x = 0, y = -32 }
+          },
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 256,
+          y = 400,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = 0, y = -32 },
+            { x = 64, y = -32 },
+            { x = 64, y = 0 },
+            { x = 64, y = -32 },
+            { x = 0, y = -32 }
+          },
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 208,
+          y = 320,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = -128, y = 0 },
+            { x = -128, y = 32 },
+            { x = -80, y = 32 },
+            { x = -80, y = 64 },
+            { x = -32, y = 64 },
+            { x = -32, y = 32 },
+            { x = 0, y = 32 }
+          },
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 368,
+          y = 432,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = 64, y = 0 },
+            { x = 0, y = 0 }
+          },
+          properties = {}
+        },
+        {
+          name = "",
+          type = "guard",
+          x = 32,
+          y = 400,
+          width = 0,
+          height = 0,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = 48, y = 0 },
+            { x = 48, y = 32 },
+            { x = 0, y = 32 },
+            { x = 32, y = 32 },
+            { x = 32, y = 0 },
+            { x = 0, y = 0 }
+          },
           properties = {}
         }
       }
